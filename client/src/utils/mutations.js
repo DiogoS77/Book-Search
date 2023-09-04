dafa3064a521cd65d40a6349fab652e771eefb49
DiogoS77 <diogo.messi.18@hotmail.com>
2023-09-04 16:36:04 +0100

    mutation.js added and completed

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,64 @@
+import {gql} from "@apollo/client";
+
+// Define your login mutation
+export const LOGIN_USER = gql`
+  mutation LoginUser($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+// Define your addUser mutation
+export const ADD_USER = gql`
+  mutation AddUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+// Define your saveBook mutation
+export const SAVE_BOOK = gql`
+  mutation SaveBook($bookData: BookInput!) {
+    saveBook(bookData: $bookData) {
+      _id
+      username
+      email
+      password
+      savedBooks {
+        _id
+        authors
+        description
+        bookId
+        image
+        link
+        title
+      }
+    }
+  }
+`;
+
+// Define your removeBook mutation
+export const REMOVE_BOOK = gql`
+  mutation RemoveBook($id: ID!) {
+    removeBook(_id: $id) {
+      _id
+      authors
+      description
+      bookId
+      image
+      link
+      createdAt
+      title
+    }
+  }
+`;
