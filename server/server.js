8c244f54b26bbecd14a82c2122154df4a8c8a6af
DiogoS77 <diogo.messi.18@hotmail.com>
2023-09-06 21:50:22 +0100

    server.js reworked

--- server/server.js
+++ server/server.js
@@ -1,33 +1,42 @@
 const express = require("express");
 const path = require("path");
-const {ApolloServer} = require("apollo-server-express"); // Import Apollo Server
-const {typeDefs, resolvers} = require("./schemas"); // Import your GraphQL schema
+const {ApolloServer} = require("apollo-server-express");
+const {typeDefs, resolvers} = require("./schemas");
 const db = require("./config/connection");
-const routes = require("./routes");
+const {authMiddleware} = require("./utils/auth");
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+async function startServer() {
+  const app = express();
+  const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
+  });
 
-// If we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  await server.start();
+  server.applyMiddleware({app});
+
+  app.use(express.urlencoded({extended: false}));
+  app.use(express.json());
+
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/build")));
+  }
+
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/"));
+  });
+
+  db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(
+        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+      );
+    });
+  });
 }
 
-app.use(routes);
-
-// Define your GraphQL schema and context
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: ({req}) => ({req}), // Pass the Express request object to context
-});
-
-// Apply Apollo Server as middleware
-server.applyMiddleware({app});
-
-db.once("open", () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+startServer();
