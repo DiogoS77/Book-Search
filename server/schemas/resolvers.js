76ef69f2ff87202324fdc7e8b60f1e67f8175d7b
DiogoS77 <diogo.messi.18@hotmail.com>
2023-09-06 22:06:11 +0100

    reworked resolver.js to fix the errors

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,119 +4,46 @@ const {signToken} = require("../utils/auth");
 
 const resolvers = {
   Query: {
-    // Get a list of all users
-    allUsers: async () => {
-      try {
-        const users = await User.find().populate("savedBooks");
-        return users;
-      } catch (error) {
-        console.error("Error fetching users:", error.message);
-        throw new Error("Failed to fetch users");
-      }
-    },
-
-    // Get a single user by username
-    getUser: async (_, {username}) => {
-      try {
-        const user = await User.findOne({username}).populate("savedBooks");
-        return user;
-      } catch (error) {
-        console.error("Error fetching user by username:", error.message);
-        throw new Error("Failed to fetch user");
-      }
-    },
-
-    // Get a list of all books
-    allBooks: async (_, {username}) => {
-      try {
-        const params = username ? {username} : {};
-        const books = await Book.find(params).sort({createdAt: -1});
-        return books;
-      } catch (error) {
-        console.error("Error fetching books:", error.message);
-        throw new Error("Failed to fetch books");
-      }
-    },
-
-    // Get a single book by bookId
-    getBook: async (_, {bookId}) => {
-      try {
-        const book = await Book.findOne({_id: bookId});
-        return book;
-      } catch (error) {
-        console.error("Error fetching book by bookId:", error.message);
-        throw new Error("Failed to fetch book");
-      }
-    },
-
-    // Get the current user (requires authentication)
-    me: async (_, __, {user}) => {
-      if (!user) {
-        throw new AuthenticationError("You need to be logged in!");
-      }
-
-      try {
-        const currentUser = await User.findOne({_id: user._id}).populate(
-          "savedBooks"
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({_id: context.user._id}).select(
+          "-__v -password"
         );
-        return currentUser;
-      } catch (error) {
-        console.error("Error fetching user from the database:", error.message);
-        throw new Error("Failed to fetch user");
+        return userData;
       }
+      throw new AuthenticationError("You are not logged in");
     },
   },
 
   Mutation: {
-    // Create a new user
-    createUser: async (_, {username, email, password}) => {
-      try {
-        const user = await User.create({username, email, password});
-        const token = signToken(user);
-        return {token, user};
-      } catch (error) {
-        console.error("Error creating user:", error.message);
-        throw new Error("Failed to create user");
-      }
+    addUser: async (parent, args) => {
+      const {username, email, password} = args;
+      const user = await User.create({username, email, password});
+      const token = signToken(user);
+      return {token, user};
     },
 
-    // User login
-    loginUser: async (_, {email, password}) => {
-      try {
-        const user = await User.findOne({email});
+    login: async (parent, {email, password}) => {
+      const user = await User.findOne({email});
 
-        if (!user) {
-          throw new AuthenticationError(
-            "No user found with this email address"
-          );
-        }
-
-        const isPasswordCorrect = await user.isCorrectPassword(password);
-
-        if (!isPasswordCorrect) {
-          throw new AuthenticationError("Incorrect credentials");
-        }
-
-        const token = signToken(user);
-
-        return {token, user};
-      } catch (error) {
-        console.error("Error during login:", error.message);
-        throw new Error("Failed to login");
-      }
-    },
-
-    // Save a book to the user's collection
-    saveBook: async (
-      _,
-      {authors, description, title, bookId, image, link},
-      {user}
-    ) => {
       if (!user) {
-        throw new AuthenticationError("You need to be logged in!");
+        throw new AuthenticationError("No user found with this email");
       }
 
-      try {
+      const correctPassword = await user.isCorrectPassword(password);
+
+      if (!correctPassword) {
+        throw new AuthenticationError("Incorrect password");
+      }
+
+      const token = signToken(user);
+
+      return {token, user};
+    },
+
+    saveBook: async (parent, args, context) => {
+      if (context.user) {
+        const {authors, description, title, bookId, image, link} = args;
         const book = await Book.create({
           authors,
           description,
@@ -127,37 +54,30 @@ const resolvers = {
         });
 
         const updatedUser = await User.findOneAndUpdate(
-          {_id: user._id},
+          {_id: context.user._id},
           {$addToSet: {savedBooks: book._id}},
           {new: true, runValidators: true}
         );
 
         return updatedUser;
-      } catch (error) {
-        console.error("Error saving book:", error.message);
-        throw new Error("Failed to save book");
       }
+
+      throw new AuthenticationError("You need to be logged in");
     },
 
-    // Remove a book from the user's collection
-    removeBook: async (_, {bookId}, {user}) => {
-      if (!user) {
-        throw new AuthenticationError("You need to be logged in!");
-      }
-
-      try {
-        const book = await Book.findOneAndDelete({_id: bookId});
-        const updatedUser = await User.findOneAndUpdate(
-          {_id: user._id},
-          {$pull: {savedBooks: bookId}},
+    removeBook: async (parent, args, context) => {
+      if (context.user) {
+        const {_id} = args;
+        const book = await Book.findOneAndDelete({_id});
+        const user = await User.findOneAndUpdate(
+          {_id: context.user._id},
+          {$pull: {savedBooks: book._id}},
           {new: true}
         );
-
         return book;
-      } catch (error) {
-        console.error("Error removing book:", error.message);
-        throw new Error("Failed to remove book");
       }
+
+      throw new AuthenticationError("You need to be logged in");
     },
   },
 };
